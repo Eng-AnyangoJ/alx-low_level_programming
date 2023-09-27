f0525bbecd4fdd80f206061f83cab5223e685bee
Eng-AnyangoJ <janettjael@gmail.com>
2023-09-27 05:39:07 -0700

    betty style error output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * Main - Entry point
+ * main - Program prints a line to the standard error
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n", 61);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
