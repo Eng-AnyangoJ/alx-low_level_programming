0912e719980546938ab6137df3f464d2fdf22ee5
Eng-AnyangoJ <janettjael@gmail.com>
2023-10-09 10:41:53 -0700

    printing 0-10 without using print f or char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Prints sigle digit no's of base 10 from 0
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
